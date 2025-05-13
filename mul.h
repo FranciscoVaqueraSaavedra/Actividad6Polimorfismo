a4612b9e8a22715d6d2ff1bd60001bb04f874cd9
FranciscoVaqueraSaavedra <141375999+FranciscoVaqueraSaavedra@users.noreply.github.com>
2025-05-12 21:34:09 -0600

    Update mul.h

--- mul.h
+++ mul.h
@@ -1,3 +1,4 @@
+
 /*
 * Copyright (C) 2025 Tec de Monterrey
  *
@@ -5,6 +6,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 
 #include <math.h>
 #include <stdexcept>
